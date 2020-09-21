e45197338699583dfb470353be303eb20e440df6
leomet07 <leomet07@gmail.com>
2020-09-21 12:13:32 -0400

    test

--- server/api/index.js
+++ server/api/index.js
@@ -1,23 +1,20 @@
 const router = require("express").Router();
 const Project = require("../models/Project");
 
-
 // Get all the cards, or search by params in request body.
 
 router.get("/", (req, res) => {
-	res.send("Api hello world")
-
-})
+	res.send("Api hello world");
+});
 router.get("/get_projects", async (req, res) => {
 	const skip = req.body.skip || 0;
 
 	const projects = await Project.find({}).skip(skip).limit(10);
 
-
 	projects.sort((a, b) => {
 		return a.index - b.index;
 	});
-	res.json(projects)
-})
+	res.json(projects);
+});
 
 module.exports.router = router;
